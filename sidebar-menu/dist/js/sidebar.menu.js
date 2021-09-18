d07b6289e6f1d38d597f02a9bf0d521f27cf8820
Liu <levy.liu@sap.com>
2021-09-18 10:49:38 +0800

    backup not ready

--- sidebar-menu/dist/js/sidebar.menu.js
+++ sidebar-menu/dist/js/sidebar.menu.js
@@ -30,11 +30,47 @@ $(() => {
     }
 
     function slideUp(target, duration = 500) {
-        $(target).attr('transitionProperty', 'height, margin, padding');
-        $(target).attr();
+        $(target).prop('transitionProperty', 'height, margin, padding');
+        $(target).prop('transitionDuration', duration + 'ms');
+        $(target).prop('boxSizing', 'border-box');
+        $(target).prop('height', target.offsetHeight + 'px');
+        $(target).prop('overflow', 'hidden');
+        $(target).prop('height', 0);
+        $(target).prop('paddingTop', 0);
+        $(target).prop('paddingBottom', 0);
+        $(target).prop('marginTop', 0);
+        $(target).prop('marginBottom', 0);
+        setTimeout(() => {
+            $(target).prop('display', 'none');
+            $(target).removeProp('height', 'padding-top', 'padding-bottom',
+                'margin-top', 'margin-bottom', 'overflow', 
+                'transition-duration', 'transition-property');
+        }, duration);
+
     }
 
     function slideDown(target, duration = 500) {
+        $(target).removeProp('display');
+        if ($(target).prop('display') == 'none') {
+            $(target).prop('display', 'block')
+        }
+
+        $(target).prop('overflow', 'hidden');
+        $(target).prop('height', 0);
+        $(target).prop('paddingTop', 0);
+        $(target).prop('paddingBottom', 0);
+        $(target).prop('marginTop', 0);
+        $(target).prop('marginBottom', 0);
+        $(target).prop('boxSizing', 'border-box');
+        $(target).prop('transitionProperty', 'height, margin, padding');
+        $(target).prop('transitionDuration', duration + 'ms');
+        $(target).prop('height', target.offsetHeight + 'px');
+        
+        $(target).removeProp('padding-top', 'padding-bottom', 'margin-top', 'margin-bottom');
+        
+        setTimeout(() => {
+            $(target).removeProp('height', 'overflow', 'transition-duration', 'transition-property');
+        }, duration);
 
     }
 });
