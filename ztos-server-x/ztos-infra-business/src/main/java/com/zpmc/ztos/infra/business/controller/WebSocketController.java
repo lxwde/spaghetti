15832ed1fee8083c1691d0d98c7d4f00a7129ab4
lxwde <lxwde@sina.com>
2022-03-01 15:28:38 +0800

    web socket controller ready

--- ztos-server-x/ztos-infra-business/src/main/java/com/zpmc/ztos/infra/business/controller/WebSocketController.java
+++ ztos-server-x/ztos-infra-business/src/main/java/com/zpmc/ztos/infra/business/controller/WebSocketController.java
@@ -1,4 +1,17 @@
 package com.zpmc.ztos.infra.business.controller;
 
+import com.zpmc.ztos.infra.business.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebSocketController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
+    @MessageMapping("/messages")
+    public void received(Message message) {
+        logger.info("web socket message received: {}", message);
+    }
 }
